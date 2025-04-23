6fe1cf51fe06b523b560df2560fe56b40b3ddf7b
ivantrevizol <ivan.trevizol@vistacx.com.br>
2025-04-23 09:33:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,8 @@
-export default { base: '/' }
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+})
